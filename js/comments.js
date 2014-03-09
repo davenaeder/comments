f34af0e511209edf77755fa8369d22260436df0a
David Naedder <davenaeder@gmail.com>
2014-03-09 09:05:04 -0400

    Comment Node and Lines Idea
    A random set of nodes all with lines connecting.

--- js/comments.js
+++ js/comments.js
@@ -1,47 +1,18 @@
-/**
- * TODO
- *
- * Externalize window event handling
- * Camera handling and functionality
- * - drag and spin example: http://localdev/threedee/threejs/examples/webgl_geometry_extrude_shapes.html
- * - normal handling (what is used initially)
- * - this should include all event listeners for camera operations
- * Shaders
- * - http://localdev/threedee/threejs/examples/webgl_custom_attributes_particles2.html
- * - http://www.html5rocks.com/en/tutorials/webgl/million_letters/
- * - blur shader: http://localdev/threedee/threejs/examples/webgl_particles_sprites.html
- * Build OOP way
- * - Comment Class (Node item)
- * Implement TweenMax where necessary
- */
 
-/**
- * Controls
- *
- * hide / show stats
- * reset camera position
- * choose camera type
- * show xyz coordinates in center
- */
+// TODO
+// - externalize window event handling
+// - externalize camera
+// - controls
+//    - hide/show stats
+//    - rotate camera / camera controls with common functionality
+// - util
+//    - random range
+// - orbit controls
+//    - consider using with momentum https://gist.github.com/paulkaplan/5770247
 
-
-/**
- * Sphere Idea
- * All comments are on perimeter of sphere. When a comment is added, the connections that its makes (the conversation) is
- * highlighted.
- */
-
-/**
- * Top Down Idea
- * Comments with no parents are displayed on top. Any child comments are displayed going down in y direction. Lines are
- * drawn to show comment connections
- */
-
-/**
- * Universe Idea
- * A conversation is a cluster (a group of comments). Within each cluster all the nodes organize themselves uniformaly (gravity / spring / repel).
- * In the 'universe' these clusters will organize themselves uniformly. Random comments will 'fill in the gaps'
- */
+// - is using blur - http://localdev/threedee/threejs/examples/webgl_particles_sprites.html
+// - camera drag and spin - http://localdev/threedee/threejs/examples/webgl_geometry_extrude_shapes.html
+// - can use shaders - http://localdev/threedee/threejs/examples/webgl_custom_attributes_particles2.html
 
 /**
  * Note: if you notice bad framerate, make sure display is set to "Best For Retina"
@@ -50,14 +21,14 @@
 if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 
 var container, stats;
-var camera, scene, renderer, particles, line, nodeMaterial, parameters, i, h, color;
+var camera, scene, renderer, particles, geometry, lineMaterial, line, nodeMaterial, parameters, i, h, color;
 var mouseX = 0, mouseY = 0, scrollPos = 0;
 
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 
 var comments = [];
-createComments(1000);
+// createComments(1000);
 
 init();
 animate();
@@ -88,8 +59,46 @@ function init() {
   container.appendChild( stats.domElement );
 
   // BUILD SCENE
-   buildSphereScene()
- // buildTopDownScene()
+
+  var sprite = THREE.ImageUtils.loadTexture( "img/textures/disc.png" );
+
+  nodeGeometry = new THREE.Geometry();
+  lineGeometry = new THREE.Geometry();
+
+  // every vertex is a particle position
+  var vertex;
+  for ( var i = 0; i < 1000; i ++ ) {
+
+    vertex = new THREE.Vector3();
+    vertex.x = Math.random() * 2 - 1;
+    vertex.y = Math.random() * 2 - 1;
+    vertex.z = Math.random() * 2 - 1;
+    vertex.normalize();
+    vertex.multiplyScalar( Math.random() * 10 + 550 );
+
+    nodeGeometry.vertices.push( vertex );
+    lineGeometry.vertices.push( vertex );
+
+  }
+
+  // lines
+  lineMaterial = new THREE.LineBasicMaterial({
+    color: 0xffffff,
+    opacity: 0.5,
+    transparent: true
+  });
+
+  line = new THREE.Line( lineGeometry, lineMaterial );
+  scene.add( line );
+
+  // build particle system
+  var nodeMaterial = new THREE.ParticleSystemMaterial( { size: 15, sizeAttenuation: false, map: sprite, transparent: true } );
+  nodeMaterial.color.setHex( 0x00ff00 );
+
+  particles = new THREE.ParticleSystem( nodeGeometry, nodeMaterial );
+  particles.sortParticles = true;
+  scene.add( particles );
+
 
   document.addEventListener( 'mousemove', onDocumentMouseMove, false );
   document.addEventListener( 'touchstart', onDocumentTouchStart, false );
@@ -105,142 +114,30 @@ function init() {
 
 }
 
-function buildTopDownScene() {
-  var sprite = THREE.ImageUtils.loadTexture( "img/textures/disc.png" );
-  var lineMaterial = new THREE.LineBasicMaterial({
-    color: 0xffffff,
-    opacity: 0.5,
-    transparent: true
-  });
-
-  var vertex;
-  var lineGeometry;
-  var nodeGeometry = new THREE.Geometry();
-
-  for ( var i = 0; i < comments.length; i ++ ) {
-
-    item = comments[i];
-
-    if(item.parentNodeId === undefined) {
-      vertex = new THREE.Vector3();
-      vertex.x = Math.random() * 2 - 1;
-      vertex.z = Math.random() * 2 - 1;
-//      vertex.normalize();
-      vertex.multiplyScalar( Math.random() * 10 + 650 );
-
-      // say top is 200
-      vertex.y = 200
-
-    } else {
-      vertex = comments[item.parentNodeId].vertex.clone()
-      vertex.x += Util.randomRange(-50, 50);
-      vertex.z += Util.randomRange(-50, 50)
-      vertex.y -= 100
-    }
-
-
-    // save vertex reference
-    comments[i].vertex = vertex;
-    nodeGeometry.vertices.push( vertex );
-
-    // add line
-    if(comments[i].parentNodeId !== undefined) {
-      lineGeometry = new THREE.Geometry();
-      lineGeometry.vertices.push( comments[ comments[i].parentNodeId ].vertex );
-      lineGeometry.vertices.push( vertex );
-
-      line = new THREE.Line( lineGeometry, lineMaterial );
-      scene.add( line );
-    }
-
-  }
-
-
-  // build particle system
-  var nodeMaterial = new THREE.ParticleSystemMaterial( { size: 15, sizeAttenuation: false, map: sprite, transparent: true } );
-  nodeMaterial.color.setHex( 0x00ff00 );
-
-  particles = new THREE.ParticleSystem( nodeGeometry, nodeMaterial );
-  particles.sortParticles = true;
-  scene.add( particles );
-
-}
-
-
-// a sphere where all nodes are positioned on outside and lines are connecting inside
-function buildSphereScene() {
-  var sprite = THREE.ImageUtils.loadTexture( "img/textures/disc.png" );
-  var lineMaterial = new THREE.LineBasicMaterial({
-    color: 0xffffff,
-    opacity: 0.5,
-    transparent: true
-  });
-
-
-  // every vertex is a particle position
-  var vertex;
-  var lineGeometry;
-  var nodeGeometry = new THREE.Geometry();
-
-  for ( var i = 0; i < comments.length; i ++ ) {
-
-    vertex = new THREE.Vector3();
-    vertex.x = Math.random() * 2 - 1;
-    vertex.y = Math.random() * 2 - 1;
-    vertex.z = Math.random() * 2 - 1;
-    vertex.normalize();
-    vertex.multiplyScalar( Math.random() * 10 + 550 );
-
-    // save vertex reference
-    comments[i].vertex = vertex;
-    nodeGeometry.vertices.push( vertex );
-
-    // add line
-    if(comments[i].parentNodeId !== undefined) {
-      lineGeometry = new THREE.Geometry();
-      lineGeometry.vertices.push( comments[ comments[i].parentNodeId ].vertex );
-      lineGeometry.vertices.push( vertex );
-
-      line = new THREE.Line( lineGeometry, lineMaterial );
-      scene.add( line );
-    }
-
-  }
-
-
-  // build particle system
-  var nodeMaterial = new THREE.ParticleSystemMaterial( { size: 15, sizeAttenuation: false, map: sprite, transparent: true } );
-  nodeMaterial.color.setHex( 0x00ff00 );
-
-  particles = new THREE.ParticleSystem( nodeGeometry, nodeMaterial );
-  particles.sortParticles = true;
-  scene.add( particles );
-
-}
-
 function createComments(count) {
   // loop and create comment objets
   var i, item;
   for(i = 0; i < count; i++) {
-
     item = {
       id: i,
-      children: [],
+      parentNode: undefined,
       message: "this is comment " + i
     };
-
-    // parent node (a conversation) - about x% of time choose a parent node
-    if(i !== 0 && (Math.random() <= 0.6)) {
-      console.log("convo")
-      item.parentNodeId = Util.randomRangeInt(0, i-1);
-
-      // update parent with data
-      comments[item.parentNodeId].children.push(i)
-    }
 
     comments.push(item);
   }
 
+  // loop and apply parent nodes (w/%)
+  // parent node can only be from a previous comment
+  for(i = 0; i < count; i++) {
+    item = comments[i];
+
+    // about 40% of time choose a parent node
+    if(i !== 0 && (Math.random() <= 0.4)) {
+      parentPosition = Util.randomRangeInt(0, i-1);
+      item.parentNode = comments[parentPosition].id;
+    }
+  }
 }
 
 function onWindowResize() {
@@ -306,7 +203,7 @@ function animate() {
 
 function render() {
 
-//  var time = Date.now() * 0.00005;
+  var time = Date.now() * 0.00005;
 
   camera.position.x += ( mouseX - camera.position.x ) * 0.05;
   camera.position.y += ( - mouseY - camera.position.y ) * 0.05;
